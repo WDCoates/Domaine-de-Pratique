033a3bb107a8dc4d6d09f7578306a3ba96b944de
W D Coates <wdcoates@users.noreply.github.com>
2018-01-12 13:04:34 +0000

    Add Main

---
+++ Prof CSharp5/07 Operators and Casts/MainProgram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleA1._07_Operators_and_Casts
+{
+    class MainProgram
+    {
+        public static void Main()
+        {
+        }
+    }
+}
