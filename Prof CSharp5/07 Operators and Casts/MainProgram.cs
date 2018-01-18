b635a819fd6f919d49c7fe68fefd18ce9b2de7c7
W D Coates <wd_coates@hotmail.com>
2018-01-18 22:57:07 +0000

    Type Con

--- Prof CSharp5/07 Operators and Casts/MainProgram.cs
+++ Prof CSharp5/07 Operators and Casts/MainProgram.cs
@@ -6,10 +6,47 @@ using System.Threading.Tasks;
 
 namespace ConsoleA1._07_Operators_and_Casts
 {
+    struct Items
+    {
+        public string Descp;
+        public int AppPrice;
+    }
     class MainProgram
     {
         public static void Main()
         {
+            //Type Cons
+            long val = 300000000000;
+            try {
+                int i = checked((int)val);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+
+            ushort c = 43;
+            char sym = (char)c;
+            Console.WriteLine(sym);
+
+            double[] Prices = { 10.90, 20.25, 15.33, 34.99 };
+
+            Items items;
+            items.Descp = "Red Bottle";
+            items.AppPrice = (int)(Prices.Average());
+
+
+            try
+            {
+                int? a = null;
+                int b = (int)a;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
         }
     }
 }
