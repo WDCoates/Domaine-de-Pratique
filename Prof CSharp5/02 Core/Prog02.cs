f648b4db9330fa3f7eee2b2a9bd525e1b632ca13
W D Coates <wdcoates@users.noreply.github.com>
2018-01-18 09:18:59 +0000

    Core Back

---
+++ Prof CSharp5/02 Core/Prog02.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Con1 = System.Console;
+
+namespace ConsoleA1._02_Core
+{
+    class Prog02
+    {
+        public static void Main()
+        {
+          Con1.WriteLine($"Using Aliases ;-)");  
+        }
+    }
+}
