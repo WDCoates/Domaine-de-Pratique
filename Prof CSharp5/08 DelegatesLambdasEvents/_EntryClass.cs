93f678ba67619161820b7688782f858fe3ccdfa3
W D Coates <wdcoates@users.noreply.github.com>
2018-02-02 08:04:21 +0000

    Mmm from Home!

--- Prof CSharp5/08 DelegatesLambdasEvents/_EntryClass.cs
+++ Prof CSharp5/08 DelegatesLambdasEvents/_EntryClass.cs
@@ -3,14 +3,13 @@
 namespace ConsoleA1._08_DelegatesLambdasEvents
 {
     class _EntryClass
-{
-    public static void Main()
     {
-        Cns.WriteLine("Chapter 08!");
+        public static void Main()
+        {
+            Cns.WriteLine("Chapter 08!");
 
-        Delegates.DemoDelegates();
 
-        Cns.ReadKey();
+            Cns.ReadKey();
+        }
     }
-}
 }
