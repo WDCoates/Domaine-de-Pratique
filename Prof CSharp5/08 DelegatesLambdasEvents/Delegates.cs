adec4f0efa38449e8afda45546dfdb6869c78c66
W D Coates <wdcoates@users.noreply.github.com>
2018-02-05 08:55:40 +0000

    Simple Delegates

--- Prof CSharp5/08 DelegatesLambdasEvents/Delegates.cs
+++ Prof CSharp5/08 DelegatesLambdasEvents/Delegates.cs
@@ -1,19 +1,62 @@
-﻿using Con = System.Console;
+﻿using System;
+using Con = System.Console;
 
 namespace ConsoleA1._08_DelegatesLambdasEvents
 {
     delegate string GetAString();
 
+    class MathOps
+    {
+        public static double TimesTwo(double val)
+        {
+            return val * 2;
+        }
+
+        public static double Sqr(double val)
+        {
+            return val * val;
+        }
+    }
+
+
+    delegate double DoubleOp(double x);
     public struct Delegates
     {
         public static void DemoDelegates()
         {
             int x = 40;
             GetAString fStringMethod = new GetAString(x.ToString);
+            GetAString sStringMethod = x.ToString;                      //Also can be done like this...
 
 
             Con.WriteLine($"x String is {fStringMethod()}");
+            Con.WriteLine($"x String is {sStringMethod()}");
+
+            //Simple Delagte Example
+            DoubleOp[] ops =
+            {
+                MathOps.TimesTwo,
+                MathOps.Sqr
+            };
+
+
+            for (int i = 0; i < ops.Length; i++)
+            {
+                Con.WriteLine($"Using ops[{ops[i].Method}]:");
+                ProAndDis(ops[i], 2.0);
+                ProAndDis(ops[i], 7.35);
+                ProAndDis(ops[i], 1.555);
+            }
+
+
         }
 
+        static void ProAndDis(DoubleOp action, double val)
+        {
+            double res = action(val);
+            Con.WriteLine($"Value is {val}, result of operation is {res}");
+        }
     }
+
+    
 }
