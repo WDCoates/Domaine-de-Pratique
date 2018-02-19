64eb177fa4bb64a9f52c08ae237053d2fab1e989
W D Coates <wdcoates@users.noreply.github.com>
2018-02-19 09:37:00 +0000

    Start BubbleSort Class

---
+++ Prof CSharp5/08 DelegatesLambdasEvents/BubbleSorter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleA1._08_DelegatesLambdasEvents
+{
+    class BubbleSorter
+    {
+        static public void Sort<t>(IList<T> sortList, Func<T, T, bool> comp)
+        {
+            bool swapped = true;
+            do
+            {
+
+            } while (swapped);
+        }
+    }
+}
