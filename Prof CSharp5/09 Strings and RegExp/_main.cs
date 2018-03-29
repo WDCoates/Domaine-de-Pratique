468aa79d758f82338fc06c490e28827dd1728d1e
W D Coates <wdcoates@users.noreply.github.com>
2018-03-29 13:02:25 +0100

    Start Chapter 09
    Strings and Regular Expressions

---
+++ Prof CSharp5/09 Strings and RegExp/_main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cons = System.Console;
+
+namespace ConsoleA1._09_Strings_and_RegExp
+{
+    public class _Main
+    {
+        public static void Main()
+        {
+            Cons.WriteLine($@"Chapter 09 Strings and Regular Expressions");
+
+            Cons.ReadKey();
+        }
+    }
+}
