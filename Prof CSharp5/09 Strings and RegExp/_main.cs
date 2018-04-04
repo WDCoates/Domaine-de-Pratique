aabfadf3d4fa3d8fff5dd75541931e0b5a97aa56
W D Coates <wdcoates@users.noreply.github.com>
2018-04-04 09:04:51 +0100

    String Builder Obj

--- Prof CSharp5/09 Strings and RegExp/_main.cs
+++ Prof CSharp5/09 Strings and RegExp/_main.cs
@@ -13,6 +13,23 @@ namespace ConsoleA1._09_Strings_and_RegExp
         {
             Cons.WriteLine($@"Chapter 09 Strings and Regular Expressions");
 
+            var test = "Test";
+            StringBuilder sbGreetings = new StringBuilder($@"Just A little note to say hello...{test}.", 150);
+            sbGreetings.AppendFormat($" This goes on the end.");
+
+            Cons.WriteLine($"The Greeting: {sbGreetings}");
+
+            //Now Encrypt
+            for (int i = 'A'; i <= 'z'; i++)
+            {
+                char f = (char)i;
+                char w = (char) (i-1);  //Ensure the sign is opposite to the loop.
+                sbGreetings = sbGreetings.Replace(f, w);
+            } 
+            Cons.WriteLine($"After Encryption: {sbGreetings}");
+
+            
+            
             Cons.ReadKey();
         }
     }
