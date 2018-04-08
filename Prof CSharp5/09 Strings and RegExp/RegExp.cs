db9787601faed70fad60b6b8b978b9b58ff313a8
W D Coates <wd_coates@hotmail.com>
2018-04-08 21:37:04 +0100

    Start RegEx

---
+++ Prof CSharp5/09 Strings and RegExp/RegExp.cs
@@ -0,0 +1,27 @@
+﻿using Cons = System.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleA1._09_Strings_and_RegExp
+{
+    public struct RegExp
+    {
+        const string myTest = @"Anything you can do I can do aswell...";
+        const string pat = "ing";
+
+        public static void runReg ()
+        {
+            MatchCollection matches = Regex.Matches(myTest, pat, RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture);
+
+
+            foreach (Match m in matches)
+            {
+                Cons.WriteLine(@"Match {i} ");
+            }
+        }
+    }
+}
