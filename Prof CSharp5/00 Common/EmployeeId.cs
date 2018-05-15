c005ab3e9710ecdd4673c6489de343fd35e138bf
DCoates <DCoates@Rubiconsoftware.com>
2018-05-15 08:56:24 +0100

    Create New EmployeeId Class

---
+++ Prof CSharp5/00 Common/EmployeeId.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleA1._00_Common
+{
+    public struct EmployeeId : IEquatable<EmployeeId>
+    {
+        private readonly char prefix;
+        private readonly int number;
+        
+        
+        public EmployeeId(string id)
+        {
+            Contract.Requires<ArgumentNullException>(id != null);
+
+            prefix = (id.ToUpper())[0];
+            int numLength = id.Length - 1;
+            try
+            {
+                number = int.Parse(id.Substring(1, numLength > 6 ? 6 : numLength));
+            }
+            catch (FormatException fE)
+            {
+                throw new EmployeeIdException("Invalid EmployeeId format!");
+            }
+        }
+
+        public override string ToString()
+        {
+            return prefix.ToString() + $"{this.number, 6:000000}";
+        }
+
+        public override int GetHashCode()
+        {
+            return (number ^ number << 16) * 0x15051505;
+        }
+
+        public bool Equals(EmployeeId other)
+        {
+            if (other == null) return false;
+            return (prefix == other.prefix && number == other.number);
+        }
+
+        public override bool Equals(Object obj)
+        {
+            return Equals((EmployeeId) obj);
+        }
+
+        public static bool operator ==(EmployeeId left, EmployeeId right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(EmployeeId left, EmployeeId right)
+        {
+            return !(left == right);
+        }
+    }
+    
+    public class EmployeeIdException : Exception
+    {
+        public EmployeeIdException (string message) : base(message) {}
+    }
+}
