4322e0e444ae2602102c0993a99d5eddd1633287
DCoates <DCoates@Rubiconsoftware.com>
2018-06-22 08:42:46 +0100

    Start LINQ stuff...

---
+++ Prof CSharp5/11 LINQ/_11Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Cons = System.Console;
+
+namespace ConsoleA1._11_LINQ
+{
+    class _11Main
+    {
+        public static void Main(string[] args)
+        {
+            Cons.WriteLine($"Start the LINQ Stuff.....{args[0]}");
+
+            Cons.ReadKey();
+        }
+    }
+}
