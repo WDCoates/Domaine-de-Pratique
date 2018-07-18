a18aad90ecd9e538634608559af5c62d8d454d90
DCoates <DCoates@Rubiconsoftware.com>
2018-07-18 08:04:09 +0100

    Lambda Zips and Zipping!

--- Prof CSharp5/11 LINQ/_11Main.cs
+++ Prof CSharp5/11 LINQ/_11Main.cs
@@ -16,7 +16,7 @@ namespace ConsoleA1._11_LINQ
         {
             Cons.WriteLine($"Start the LINQ Stuff.....{args[0]}");
             var champs = Formula1.GetChampions();
-            var q1 = from r in Formula1.GetChampions() where r.Country == "British" orderby r.Wins descending select r;
+            var q1 = from r in Formula1.GetChampions() where r.Country == "UK" orderby r.Wins descending select r;
 
             foreach (Racer r in q1)
             {
@@ -29,7 +29,7 @@ namespace ConsoleA1._11_LINQ
 
             //Q1 using extensions methods to for show...
             var champs2 = new List<Racer>(Formula1.GetChampions());
-            IEnumerable<Racer> Brits = champs2.Where(c => c.Country == "British").OrderBy(o => o.LastName)
+            IEnumerable<Racer> Brits = champs2.Where(c => c.Country == "UK").OrderBy(o => o.LastName)
                 .Select(r => r);
 
             DeferredDemo.DeferredEx1();
@@ -177,6 +177,39 @@ namespace ConsoleA1._11_LINQ
             {
                 Cons.WriteLine(r.LastName);
             }
+
+            //Zip method!
+            var rNames = from r in Formula1.GetChampions()
+                where r.Country == "Italy"
+                orderby r.Wins descending
+                select new { Name = r.FirstName + "," + r.LastName};
+
+            var rNameStarts = from r in Formula1.GetChampions()
+                where r.Country == "Italy"
+                orderby r.Wins descending 
+                select new {LastName = r.LastName, Starts = r.Starts};
+
+            var zRacers = rNames.Zip(rNameStarts, (a, b) => a.Name + ", Starts: " + b.Starts);
+            foreach (var a in zRacers)
+            {
+                Cons.WriteLine(a.ToString());
+            }
+
+            //Why the selections and order are so important!
+            rNameStarts = from r in Formula1.GetChampions()
+                where r.Country == "UK"
+                orderby r.Wins
+                select new {LastName = r.LastName, Starts = r.Starts};
+            zRacers = rNames.Zip(rNameStarts, (a, b) => a.Name + ", Starts: " + b.Starts);
+            foreach (var a in zRacers)
+            {
+                Cons.WriteLine(a.ToString());
+            }
+            //Complete crap lol
+
+            //Partitioning
+
+
 
             Cons.ReadKey();
         }
