e4671092da7d7906afc5ef70bf7622b1731aca2d
W D Coates <wd_coates@hotmail.com>
2018-07-29 17:59:07 +0100

    Main created.

---
+++ Prof CSharp5/12 Dynamic Language Extensions/_12Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cons = System.Console;
+
+namespace ConsoleA1._12_Dynamic_Language_Extensions
+{
+    internal class _12Main
+    {
+        public static void Main(string[] args)
+        {
+            Cons.WriteLine($"Start Dynamic Language Extensions.");
+
+            Cons.WriteLine($"Press Any Key To Exit.");
+        }
+    }
+}
