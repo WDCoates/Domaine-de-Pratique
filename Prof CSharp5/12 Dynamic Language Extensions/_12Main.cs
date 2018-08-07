e6f75f7752526da3625c36aa2ccb6c54f095bce9
DCoates <DCoates@Rubiconsoftware.com>
2018-08-07 08:53:36 +0100

    Revert "Revert "dynamic changling!""
    This reverts commit 205aae1666f8bb1706eae7cf5209d2e02673ce8f.

---
+++ Prof CSharp5/12 Dynamic Language Extensions/_12Main.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using ConsoleA1._00_Common;
+using Cons = System.Console;
+
+namespace ConsoleA1._12_Dynamic_Language_Extensions
+{
+    class _12Main
+    {
+        public static void Main(string[] args)
+        {
+            Cons.WriteLine("Start Dynamic Language Extensions! - Checking only done at runtime.");
+
+            var sPerson = new Person();
+            dynamic dyPerson = new Person();
+
+            //sPerson.GetFullName("John", "Smithes");   //would not get past compiler.
+            try
+            {
+                var fName = dyPerson.GetFullName("This", "Is!");
+            }
+            catch (Exception ex)
+            {
+                Cons.WriteLine($"No method: {ex.Message}");
+            }
+
+            //Changing Type completly not just casting it!
+
+            dynamic dyn;
+            dyn = 100;
+            Cons.WriteLine($"dyn type: {dyn.GetType()}, Value: {dyn}");
+
+            dyn = "I'm a changling, see me change!";
+            Cons.WriteLine($"dyn type: {dyn.GetType()}, Value: {dyn}");
+
+            Person p = new Person() {FirstName = "Fme", LastName = "Lme"};
+            var f = p.FullNameToString();
+            var t = p.ToString();
+            
+            dyn = new Person() {FirstName = "Dave", LastName = "Testy"};
+            try
+            {
+                Cons.WriteLine($"dyn type: {dyn.GetType()}, Value: {dyn.FullNameToString()}");
+            }
+            catch (Exception ex)
+            {
+                Cons.WriteLine($"dyn type: {dyn.GetType()}, Value: {dyn.ToString()}");
+            }
+
+
+
+            Cons.ReadKey();
+        }
+    }
+}
