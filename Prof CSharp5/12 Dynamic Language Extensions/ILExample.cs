f818d034c298fec5adc5e99807dbbff5b109d4eb
DCoates <DCoates@Rubiconsoftware.com>
2018-08-10 17:48:51 +0100

    Dynamics

---
+++ Prof CSharp5/12 Dynamic Language Extensions/ILExample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleA1._12_Dynamic_Language_Extensions
+{
+    class IlExample
+    {
+        static void Main(string[] args)
+        {
+            StaticClass sClass = new StaticClass();
+            //DynamicClass dClass = new DynamicClass();
+
+            Console.WriteLine($"Static Class Value: {sClass.IntValue}");
+            //Console.WriteLine($"Dynamic Class Value: {dClass.DynValue}");
+            var test = Console.ReadLine();
+        }
+    }
+
+    class StaticClass
+    {
+        public int IntValue = 100;
+    }
+
+    class DynamicClass
+    {
+        public dynamic DynValue = 100;
+    }
+}
