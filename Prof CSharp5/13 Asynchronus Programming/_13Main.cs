209a41dfea889f01e0f1efc2b958891fa2493500
DCoates <DCoates@Rubiconsoftware.com>
2018-08-24 08:37:48 +0100

    Start Chapter 13 Asynchronus Programming.

---
+++ Prof CSharp5/13 Asynchronus Programming/_13Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cons=System.Console;
+
+namespace ConsoleA1._13_Asynchronus_Programming
+{
+    class _13Main
+    {
+        public static void Main(string[] args)
+        {
+            Cons.WriteLine($"Start Chapter 13 Asynchronus Programming");
+
+
+            Cons.ReadKey();
+        }
+    }
+}
