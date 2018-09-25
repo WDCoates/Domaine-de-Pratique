ad822107204b9dad68d6c8e76ba8ba27a32f9f83
DCoates <DCoates@Rubiconsoftware.com>
2018-09-25 09:04:41 +0100

    Catching exceptions...

--- Prof CSharp5/13 Asynchronus Programming/AFoundationsOfAsync.cs
+++ Prof CSharp5/13 Asynchronus Programming/AFoundationsOfAsync.cs
@@ -109,12 +109,70 @@ namespace ConsoleA1._13_Asynchronus_Programming
         {
             try
             {
-                await ThrowUpAfter(4000, "Second Throw Up!"); //Won't be caught as in Try Catch Block because try catch has fnished...
+                await ThrowUpAfter(4000, "Second Throw Up!"); 
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+
+            try
+            {
+                await ThrowUpAfter(4000, "1st M4 Throw Up!"); 
+                await ThrowUpAfter(1000, "2nd M1 Throw Up!"); //This is never got to becase the first lands in the catch block.
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Task t1 = ThrowUpAfter(4000, "T1 M4 Throw Up!");
+                Task t2 = ThrowUpAfter(1000, "T2 M4 Throw Up!");
+                await Task.WhenAll(t1, t2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Handled {0}", ex.Message);
+            }
+
+            //Aggregate Exception Information
+            Task tRes = null;
+            try
+            {
+                Task t1 = ThrowUpAfter(4000, "T1 M4 Throw Up!");
+                Task t2 = ThrowUpAfter(1000, "T2 M4 Throw Up!");
+                await (tRes = Task.WhenAll(t1, t2));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Handle all exceptions. {0}", ex.GetHashCode());
+                foreach (var ie in tRes.Exception.InnerExceptions)
+                {
+                    Console.WriteLine("Inner exception {0}", ie.Message);
+                }
+                
+            }
+
+            Task et1 = null;
+            Task et2 = null;
+            try
+            {
+                et1 = ThrowUpAfter(4000, "T1 M4 Throw Up!");
+                et2 = ThrowUpAfter(1000, "T2 M4 Throw Up!");
+                Task r = await Task.WhenAny(et1, et2);
+                if (r.IsFaulted)
+                {
+                    Console.WriteLine("? WTF {0}", r.Exception?.InnerException);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Handled {0}", ex.Message);
+            }
+
+
         }
     }
 }
