6598ebd50265ae2d64ea0bc85c1ee4d96a4b4687
DCoates <DCoates@Rubiconsoftware.com>
2018-09-26 08:57:35 +0100

    Set-up of Chap 14

---
+++ Prof CSharp5/14 Memory and Pointers/_Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Cons = System.Console;
+
+namespace ConsoleA1._14_Memory_and_Pointers
+{
+    class _Main
+    {
+        public static void Main()
+        {
+            Cons.WriteLine($"What is Memoery Management and all these Pointers all about!");
+
+
+            Cons.WriteLine($"Press anykey to exit!");
+            Cons.ReadKey();
+        }
+    }
+}
