6d7c5eeae766abd96a813c324a7b6f308106f400
DCoates <DCoates@Rubiconsoftware.com>
2018-10-03 09:22:56 +0100

    Playin with SelfDestrutors and IDisposable

---
+++ Prof CSharp5/14 Memory and Pointers/Scope1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Cons = System.Console;
+
+namespace ConsoleA1._14_Memory_and_Pointers
+{
+    class Scope1
+    {
+        public void level1()
+        {
+
+            SelfDestructor sd = null;
+            
+            try
+            {
+                sd = new SelfDestructor();    
+                sd.Up();
+                
+            }
+            catch (Exception e)
+            {
+                Cons.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sd != null)
+                {
+                    sd.Dispose();
+                }
+            }
+        }
+    }
+}
--- Prof CSharp5/14 Memory and Pointers/_Main.cs
+++ Prof CSharp5/14 Memory and Pointers/_Main.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,16 @@ namespace ConsoleA1._14_Memory_and_Pointers
             Cons.WriteLine($"What is Memoery Management and all these Pointers all about!");
 
             GC.Collect();
+            
+            Scope1 s = new Scope1();
+            s.level1();
+
+           
+            GC.Collect();
+            GC.Collect(); //~Destructors need two passes!
+
+
+            GC.Collect(10, mode: GCCollectionMode.Forced);
 
             Cons.WriteLine($"Press anykey to exit!");
             Cons.ReadKey();
