312574384792b29b57a69518e083c871dec910f6
DCoates <DCoates@Rubiconsoftware.com>
2018-10-15 09:01:03 +0100

    Set-up Reflection

---
+++ Prof CSharp5/15-Reflection/_Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cons = System.Console;
+namespace ConsoleA1._15_Reflection
+{
+    class _Main
+    {
+        public static void Main()
+        {
+            Cons.WriteLine($"Lets look at Reflection...");
+
+
+            Cons.WriteLine($"Press any key to exit.");
+            Cons.ReadKey();
+        }
+    }
+}
