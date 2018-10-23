917d5ff8baa4ccfa70a0b6109ca212d434872f17
W D Coates <24655308+WDCoates@users.noreply.github.com>
2018-10-23 08:24:13 +0100

    14 memory and pointers (#14)
    * Set-up of Chap 14
    
    * Simple Garbage Collect method.
    
    * Playin with SelfDestrutors and IDisposable
    
    * using {} to force Dispose method to fire.
    
    * Memoery Things
    
    * More Pointer stuff around displaying...
    
    * Unsafe Fixed and Fast Arrays
    
    * SupperFastArray BUFFER_OVERRUN

---
+++ Prof CSharp5/14 Memory and Pointers/Scope1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Cons = System.Console;
+
+namespace ConsoleA1._14_Memory_and_Pointers
+{
+    class Scope1
+    {
+        public void level1()
+        {
+
+            SelfDestructor sd = null;
+            
+            try
+            {
+                sd = new SelfDestructor();    
+                sd.Up();
+                
+            }
+            catch (Exception e)
+            {
+                Cons.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sd != null)
+                {
+                    sd.Dispose();
+                }
+            }
+
+            //Ensures Dispose is used on closing, },  brace.
+            using (SelfDestructor sd2 = new SelfDestructor())
+            {
+                sd2.Up();
+            }
+
+        }
+    }
+}
---
+++ Prof CSharp5/14 Memory and Pointers/SelfDestructor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleA1._14_Memory_and_Pointers
+{
+    public class SelfDestructor: IDisposable
+    {
+        public SelfDestructor()
+        {
+            Console.WriteLine(@"Here we go!");
+        }
+
+        public void Up()
+        {
+            Console.WriteLine(@"Up!");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine(@"Is this better now!");
+        }
+
+        ~SelfDestructor()
+        {
+            Console.WriteLine(@"And I'm Gone....");
+        }
+
+    }
+}
