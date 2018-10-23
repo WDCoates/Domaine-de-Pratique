29cec7674d73bfbc80d5b1c948f20a44b2a5c415
WDCoates <WD_Coates@Hotmail.com>
2018-10-23 08:46:01 +0100

    Set-up

---
+++ Prof CSharp5/16-ErrorAndExceptions/_Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleA1._16_ErrorAndExceptions
+{
+    class _Main
+    {
+        public static void Main()
+        {
+            Console.WriteLine($"Error and Exceptions.");
+
+            Console.WriteLine($"Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
