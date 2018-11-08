693436cca6fbf1ad968296410395ba96a1b0574c
WDCoates <WD_Coates@Hotmail.com>
2018-11-08 08:28:37 +0000

    OK

---
+++ Prof CSharp5/17-VSTools/_Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleA1._17_VSTools
+{
+    class _Main
+    {
+        public static void Main(string [] args)
+        {
+            Console.WriteLine("Some refactoring!");
+
+
+
+        }
+    }
+
+    public class car
+    {
+        private string colour;
+        public string Style { get; }
+
+        public string Colour
+        {
+            get { return colour; }
+            set { colour = value; }
+        }
+
+        public int Go()
+        {
+            int speed = 100;
+            return speed;
+        }
+    }
+}
