8bddfd4191a8cdfc1bd74a21819e01157aceaa4d
WDCoates <WD_Coates@Hotmail.com>
2018-11-14 17:34:27 +0000

    InterFace Testing

--- Prof CSharp5/17-VSTools/_Main.cs
+++ Prof CSharp5/17-VSTools/_Main.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Microsoft.Ajax.Utilities;
 
 namespace ConsoleA1._17_VSTools
@@ -17,7 +18,11 @@ namespace ConsoleA1._17_VSTools
             string wine = "Red";
             WineSampler wSam = new WineSampler(wine);
             var res = wSam.GetWineSample("Leather", "Blackberry");
-            
+
+            var wines = wSam.WinesByCountry("Italy");
+
+            IChampionLoader iChaps = new IChaps();
+
         }
     }
 
@@ -46,10 +51,57 @@ namespace ConsoleA1._17_VSTools
                 throw new ArgumentNullException($"Mouth must taste something!");
             }
 
+
             return wine + nose + mouth;
         }
 
+        public XElement WinesByCountry(string country)
+        {
+            XElement vintages = XElement.Load("http://www.cninnovation.com/downloads/racers.xml");
+            var q = from r in vintages.Elements("Racer")
+                where r.Element("Country").Value == country
+                select new XElement("Wines",
+                    new XAttribute("Name", r.Element("Firstname").Value + " " + r.Element("Lastname").Value));
+
+            return new XElement("Wines", q.ToArray());
+
+        }
+
+        private readonly IChampionLoader cLoader;
+
+        public WineSampler(IChampionLoader loader)
+        {
+            this.cLoader = loader;
+        }
+
+        public XElement ChampionsByCountry(string country)
+        {
+            XElement vintages = cLoader.GetChampions();
+            var q = from r in vintages.Elements("Racer")
+                where r.Element("Country").Value == country
+                select new XElement("Racers",
+                    new XAttribute("Name", r.Element("Firstname").Value + " " + r.Element("Lastname").Value));
+
+            return new XElement("Driver", q.ToArray());
+
+        }
+
+
     }
+
+    public interface IChampionLoader
+    {
+        XElement GetChampions();
+    }
+
+    public class TIhaps : IChampionLoader
+    {
+        public XElement GetChampions()
+        {
+            return XElement.Load("http://www.cninnovation.com/downloads/racers.xml");
+        }
+    }
+
 
     public class car
     {
