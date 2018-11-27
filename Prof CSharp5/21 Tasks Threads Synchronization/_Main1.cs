2f344eddaa7b394119616227e5bada4edee1ef18
WDCoates <WD_Coates@Hotmail.com>
2018-11-27 09:06:48 +0000

    Simple Parallel Loop

---
+++ Prof CSharp5/21 Tasks Threads Synchronization/_Main1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using con = System.Console;
+
+namespace ConsoleA1._21_Tasks_Threads_Synchronization
+{
+    class _Main1
+    {
+        public static void Main(string[] args)
+        {
+            con.WriteLine($"Start Looping with Parallel.For!");
+
+            ParallelLoopResult pRes = Parallel.For(0, 10, i =>
+            {
+                Thread.Sleep(10000 - i*1000);
+                con.WriteLine($"Iteration {i}, Task: {Task.CurrentId}, Thread: {Thread.CurrentThread.ManagedThreadId}");
+            });
+
+            con.WriteLine($"Completed All Treads! {pRes.IsCompleted}");
+
+            con.ReadLine();
+        }
+    }
+}
