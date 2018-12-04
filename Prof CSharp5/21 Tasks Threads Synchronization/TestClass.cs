91846cf859a3147e1f7aefea66f0ee25073b9597
W D Coates <wd_coates@hotmail.com>
2018-12-04 22:39:01 +0000

    Only Staged

---
+++ Prof CSharp5/21 Tasks Threads Synchronization/TestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleA1._21_Tasks_Threads_Synchronization
+{
+    class TestClass
+    {
+    }
+}
