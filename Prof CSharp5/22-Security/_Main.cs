e2a14d522ec8b859761a726216e1405248fd815d
WDCoates <WD_Coates@Hotmail.com>
2019-01-10 09:35:05 +0000

    Start Security

---
+++ Prof CSharp5/22-Security/_Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleA1._22_Security
+{
+    public class _Main
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"This is all about Security!");
+
+            Console.Write($"Thank you.");
+            Console.ReadLine();
+        }
+    }
+}
