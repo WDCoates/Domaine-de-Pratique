9093f6b02748d79d38c0f2a2c39cd0abe27c5ddf
WDCoates <WD_Coates@Hotmail.com>
2019-01-11 17:33:36 +0000

    Start of looking at security, principle identity and roles.

--- Prof CSharp5/22-Security/_Main.cs
+++ Prof CSharp5/22-Security/_Main.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.Security.Claims;
+using System.Security;
+using System.Security.Permissions;
+using System.Security.Principal;
+
 using System.Collections.Generic;
 using System.Linq;
+
 using System.Text;
 using System.Threading.Tasks;
+
+using con = System.Console;
 
 namespace ConsoleA1._22_Security
 {
@@ -12,8 +20,33 @@ namespace ConsoleA1._22_Security
         {
             Console.WriteLine($"This is all about Security!");
 
+            AppDomain.CurrentDomain.SetPrincipalPolicy(PrincipalPolicy.WindowsPrincipal);
+            var principal = WindowsPrincipal.Current as WindowsPrincipal;
+            var identity = principal.Identity as WindowsIdentity;
+
+            con.WriteLine($"Identity Type: {identity}");
+            con.WriteLine($"Name: {identity.Name}");
+
+            //Declarative Role-Based Security
+            try
+            {
+                ShowMessage();
+            }
+            catch (SecurityException sE)
+            {
+                con.WriteLine($"Exception Message: {sE.Message}");
+            }
+
+
             Console.Write($"Thank you.");
             Console.ReadLine();
+        }
+        
+        [PrincipalPermission(SecurityAction.Demand, Role = "BUILTIN\\Devs")]
+        static void ShowMessage()
+        {
+            con.WriteLine($"Current principal is logged in locally");
+            con.WriteLine($"(Member of local User Group)");
         }
     }
 }
