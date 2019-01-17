5195142c51c068517e34a82a8416e506a337deaf
WDCoates <WD_Coates@Hotmail.com>
2019-01-17 17:12:22 +0000

    May through this away!

---
+++ WcfService2/Membership.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Web.Security;
+
+namespace Security.Prov
+{
+    public class Membership: MembershipProvider
+    {
+        private Dictionary<string, string> users = new Dictionary<string, string>();
+        internal static string ManagerUserName = "Manager".ToLowerInvariant();
+        internal static string EmployeeUserName = "Employee".ToLowerInvariant();
+
+        public override void Initialize(string name, NameValueCollection config)
+        {
+            users.Add(ManagerUserName, "secret@Pa$$w0rd");
+            users.Add(EmployeeUserName, "secret@Pa$$w0rd");
+
+            base.Initialize(name, config);
+        }
+
+        public override bool ValidateUser(string username, string password)
+        {
+            if (users.ContainsKey(username.ToLowerInvariant()))
+            {
+                return password.Equals(users[username.ToLowerInvariant()]);
+            }
+            return false;
+        }
+
+        public override MembershipUser CreateUser(string username, string password, string email, string passwordQuestion,
+            string passwordAnswer, bool isApproved, object providerUserKey, out MembershipCreateStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ChangePasswordQuestionAndAnswer(string username, string password, string newPasswordQuestion,
+            string newPasswordAnswer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ChangePassword(string username, string oldPassword, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ResetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateUser(MembershipUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+
+        public override bool UnlockUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(object providerUserKey, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(string username, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetUserNameByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteUser(string username, bool deleteAllRelatedData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection GetAllUsers(int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetNumberOfUsersOnline()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection FindUsersByName(string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool EnablePasswordRetrieval { get; }
+        public override bool EnablePasswordReset { get; }
+        public override bool RequiresQuestionAndAnswer { get; }
+        public override string ApplicationName { get; set; }
+        public override int MaxInvalidPasswordAttempts { get; }
+        public override int PasswordAttemptWindow { get; }
+        public override bool RequiresUniqueEmail { get; }
+        public override MembershipPasswordFormat PasswordFormat { get; }
+        public override int MinRequiredPasswordLength { get; }
+        public override int MinRequiredNonAlphanumericCharacters { get; }
+        public override string PasswordStrengthRegularExpression { get; }
+    }
+}
---
+++ WcfService2/RoleProv.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+
+namespace Security.Prov
+{
+    public class Roles: RoleProvider
+    {
+        internal static string ManagerRoleName = "Manager".ToLowerInvariant();
+        internal static string EmployeeRoleName = "Employee".ToLowerInvariant();
+
+        public override void Initialize(string name, NameValueCollection config)
+        {
+            base.Initialize(name, config);
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            string[] roles = GetRolesForUser(username);
+            foreach (var role in roles)
+            {
+                if (string.Equals(role, roleName))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            if (string.Compare(username, Membership.ManagerUserName, true) == 0)
+            {
+                return new string[] {ManagerRoleName};
+            } 
+            else if(string.Compare(username, Membership.EmployeeUserName, true) == 0)
+            {
+                return new string[] {EmployeeRoleName};
+            }
+            else
+            {
+                return new string[0];
+            }
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ApplicationName { get; set; }
+    }
+}
