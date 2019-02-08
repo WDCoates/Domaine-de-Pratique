76182317bd99a20454ad771bdcd23a093177abab
WDCoates <WD_Coates@Hotmail.com>
2019-02-08 08:47:38 +0000

    Basic Setup

---
+++ Prof CSharp5/24-MaipulatingFiles/_Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using con = System.Console;
+
+namespace ConsoleA1._24_MaipulatingFiles
+{
+    public class _Main
+    {
+        public static void Main(string[] args)
+        {
+            con.WriteLine($"Looking at string manipulation!");
+            
+
+            con.ReadLine();
+        }
+    }
+}
