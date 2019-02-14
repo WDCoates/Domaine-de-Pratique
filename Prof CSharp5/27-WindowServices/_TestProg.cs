7f109d21c3667fc3023a136e65c91077b52e029f
WDCoates <WD_Coates@Hotmail.com>
2019-02-14 09:16:12 +0000

    ReWrritten to .NETStandard!

---
+++ Prof CSharp5/27-WindowServices/_TestProg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace QuoteServer
+{
+    public class _TestProg
+    {
+        public static void Main()
+        {
+            var qs = new QuoteServer(@".\MyQuotes.txt", 4444);
+            qs.Start();
+            Console.WriteLine("Press any key to exit!");
+            Console.ReadKey();
+            qs.Stop();
+        }
+    }
+}
