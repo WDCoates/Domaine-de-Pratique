90a3dc6ba1691831e1a385a0cbed0ae1d9b0a9ff
WDCoates <WD_Coates@Hotmail.com>
2019-02-14 09:02:52 +0000

    Quotes Service Server!

---
+++ QuotesServer/_TestProg.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuotesServer
+{
+    public class _TestProg
+    {
+        public static void Main()
+        {
+            var qs = new QuoteServer(@".\MyQuotes.txt", 4444);
+            qs.Start();
+            Console.WriteLine("Press any key to exit!");
+            Console.ReadKey();
+            qs.Stop();
+        }
+    }
+}
