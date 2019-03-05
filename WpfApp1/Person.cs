2212430c19da0086f640ac9c5fae813bf30c9de9
WDCoates <WD_Coates@Hotmail.com>
2019-03-05 08:23:54 +0000

    Local class Person added.

---
+++ WpfApp1/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Common
+{
+    public class Person
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public int Id { get; private set; }
+
+        public Person()
+        {
+
+        }
+    }
+}
