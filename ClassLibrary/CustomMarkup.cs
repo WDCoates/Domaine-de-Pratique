9217bd7e046650c28903923dc98db33466b8e6ae
WDCoates <WD_Coates@Hotmail.com>
2019-03-18 09:17:21 +0000

    Start Markup Extension Class

---
+++ ClassLibrary/CustomMarkup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace ClassLibrary
+{
+    class CustomMarkup : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
