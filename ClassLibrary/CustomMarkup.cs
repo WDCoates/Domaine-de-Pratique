a99d9c3c52e4c6965501b46bed63ffd25e4bc376
WDCoates <WD_Coates@Hotmail.com>
2019-03-19 08:45:43 +0000

    Custom Markup Extensions

--- ClassLibrary/CustomMarkup.cs
+++ ClassLibrary/CustomMarkup.cs
@@ -1,14 +1,55 @@
 ﻿using System;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Markup;
 
 namespace ClassLibrary
 {
-    class CustomMarkup : MarkupExtension
+    public enum Operation
     {
-        public override object ProvideValue(IServiceProvider serviceProvider)
+        Add,
+        Subtract,
+        Multiply,
+        Divide
+    }
+    [MarkupExtensionReturnType(typeof(string))]
+    public class CustomMarkup : MarkupExtension
+    {
+        public CustomMarkup()
         {
-            throw new NotImplementedException();
+        }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public Operation Operation { get; set; }
+        public override object ProvideValue(IServiceProvider sPro)
+        {
+            if (sPro.GetService(typeof(IProvideValueTarget)) is IProvideValueTarget pValue)
+            {
+                var host = pValue.TargetObject as FrameworkElement;
+                var prop = pValue.TargetProperty as DependencyProperty;
+            }
+
+            double res = 0;
+
+            switch (Operation)
+            {
+                case Operation.Add:
+                    res = X + Y;
+                    break;
+                case Operation.Subtract:
+                    res = X - Y;
+                    break;
+                case Operation.Multiply:
+                    res = X * Y;
+                    break;
+                case Operation.Divide:
+                    res = Math.Abs(Y) > 0 ? 0 : X + Y;
+                    break;
+                default:
+                    throw new ArgumentException("Invalid Operation Request");
+            }
+
+            return res.ToString(CultureInfo.InvariantCulture);
         }
     }
 }
