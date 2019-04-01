60e5b940c6d0548104eb9a95b9e462279d87240d
WDCoates <WD_Coates@Hotmail.com>
2019-04-01 09:18:06 +0100

    Convention-Based Part Reg with a little refactoring of CalculatorLoop.

---
+++ 30-MEF/CalculatorContract/Statics.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorContract
+{
+    public class Statics
+    {
+        public static void CalculatorLoop(ICalculator Calculator)
+        {
+            var operations = Calculator.GetOperations();
+            var oprDir = new SortedList<string, IOperation>();
+
+            foreach (var item in operations)
+            {
+                oprDir.Add(item.Name, item);
+                Console.WriteLine($"Name: {item.Name}, number of operands: {item.NumberOperands}");
+            }
+            Console.WriteLine();
+
+            string selectedOp = null;
+            do
+            {
+                try
+                {
+                    Console.Write($"Operation? ");
+                    selectedOp = Console.ReadLine();
+                    if (selectedOp != null && !oprDir.ContainsKey(selectedOp))
+                        continue;
+                    var operation = oprDir[selectedOp];
+                    double[] operands = new double[operation.NumberOperands];
+
+                    for (int i = 0; i < operation.NumberOperands; i++)
+                    {
+                        Console.Write($"\t operand {i + 1}? ");
+                        string selectedOperand = Console.ReadLine();
+                        operands[i] = double.Parse(selectedOperand);
+                    }
+
+                    Console.WriteLine($"Calling Calculater.....");
+                    double res = Calculator.Operate(operation, operands);
+                    Console.WriteLine($"Result: {res}");
+                }
+                catch (FormatException fe)
+                {
+                    Console.WriteLine($"Error: {fe.Message}");
+                }
+            } while (selectedOp.ToLower() != "x");
+        }
+
+    }
+}
---
+++ 30-MEF/_ProgramNE.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Registration;
+using CalculatorContract;
+
+namespace _30_MEF
+{
+    public class _ProgramNE
+    {
+        public ICalculator Calculator { get; set;}
+
+        public static void Main()
+        {
+            var pNE = new _ProgramNE();
+            pNE.Run();
+        }
+
+        public void Run()
+        {
+            var conventions = new RegistrationBuilder();
+            conventions.ForTypesDerivedFrom<ICalculator>().Export<ICalculator>();
+            conventions.ForType<_ProgramNE>().ImportProperty<ICalculator>(p => p.Calculator);
+
+            var catalog = new DirectoryCatalog(@"..\..\SimpleCalculator\Bin\Debug", conventions);
+
+            using (CompositionService cS = catalog.CreateCompositionService())
+            {
+                cS.SatisfyImportsOnce(this, conventions);
+            }
+
+            Statics.CalculatorLoop(Calculator);
+            
+        }
+    }
+}
